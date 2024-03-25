f8fae34471ad88699ef8548dc1f6bb0921cb524b
bart1012 <36210581+bart1012@users.noreply.github.com>
2024-03-25 11:48:51 +0000

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './main.js',  // Replace with your entry file
+    entry: 'main.js',  // Replace with your entry file
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
